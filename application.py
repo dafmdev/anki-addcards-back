b2c8e9b3aaab504e5023b8d02ff1ad6dd36c3570
Daniel Franco <dfranco123@hotmail.com>
2021-11-03 11:17:45 -0500

    eliminar crear cartas

--- application.py
+++ application.py
@@ -10,10 +10,8 @@ method_handler = MethodHandler()
 # Create the input class
 class CreateCard(BaseModel):
     text: str = Query(None, min_length=1, max_length=50, regex="^[a-zA-ZñáéíóúÁÉÍÓÚ\s']+$")
-    desk_name: str = Query(None, min_length=3, max_length=50, regex="^[a-zA-Z]+(:{2}[a-zA-Z]+)?$")
     language: str = Query(None, min_length=2, max_length=2, regex="^(es|en)$")
-    ipa_shape: str = Query(None, min_length=2, max_length=10, regex="^(cmu|lexico)$")
-    recipe: Dict[str, bool]
+    config: Dict[str, bool]
 
 
 # Create an api root
@@ -27,9 +25,7 @@ def read_root():
 def create_card(params: CreateCard):
     parameters = {
         'text': params.text,
-        'desk_name': params.desk_name,
-        'language': params.language,
-        'ipa_shape': params.ipa_shape
+        'language': params.language
     }
-    response = method_handler.detect_modules(parameters, params.recipe)
+    response = method_handler.detect_modules(parameters, params.config)
     return response
--- manager/method_handler.py
+++ manager/method_handler.py
@@ -1,7 +1,6 @@
 from typing import Dict
 from manager.IPA.ipa import CreateIPA
 from manager.translator.translator import CreateText
-from manager.anki_card.anki_card import CreateCard
 from manager.polly.polly import CreateSound
 
 
@@ -10,10 +9,9 @@ class MethodHandler:
     def __init__(self):
         self.create_ipa = CreateIPA()
         self.create_text = CreateText()
-        self.create_card = CreateCard()
         self.create_sound = CreateSound()
 
-    def format_response(self, text_en: str, text_es: str, text_ipa: str, url_file_polly: str, status_create_card: str) -> dict:
+    def format_response(self, text_en: str, text_es: str, text_ipa: str, url_file_polly: str) -> dict:
 
         response = {}
         if text_en:
@@ -24,20 +22,16 @@ class MethodHandler:
             response['ipa'] = text_ipa
         if url_file_polly:
             response['url_file_polly'] = url_file_polly
-        if status_create_card:
-            response['create_card'] = status_create_card
 
         return response
 
 
     def detect_modules(self, info: Dict[str, str], recipe: Dict[str, bool]) -> dict:
-        create_card = recipe.get('create_card', False)
         text_en: str = ""
         text_es: str = ""
         text_ipa: str = ""
         url_file_polly: str = ""
         name_file_polly: str = ""
-        status_create_card: str = ""
 
         if recipe['translate'] is True:
             text_en, text_es = self.create_text.create_text(info['text'], info['language'])
@@ -45,8 +39,6 @@ class MethodHandler:
             text_ipa = self.create_ipa.create_ipa(info['ipa_shape'], text_en)
         if recipe['polly'] is True:
             url_file_polly, name_file_polly = self.create_sound.create_sound(info['text'])
-        if create_card is True:
-            status_create_card = self.create_card.create_card(info['desk_name'], text_en, text_es, text_ipa, url_file_polly, name_file_polly)
 
-        return self.format_response(text_en, text_es, text_ipa, url_file_polly, status_create_card)
+        return self.format_response(text_en, text_es, text_ipa, url_file_polly)
 
